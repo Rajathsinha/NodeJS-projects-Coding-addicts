b105156f301a2b0ad4519903a18cb9be85d2d7d3
rajath sinha <sinha.rajath0@gmail.com>
2023-11-22 12:37:57 +0530

    first commit

---
+++ 03-task-manager/starter/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express();
+
+const tasks = require('./routes/tasks')
+
+
+//middleware
+
+app.use(express.json())
+
+
+
+
+//ROUTES
+
+app.get('/hello',(req,res)=>{
+    res.send('Task manager app')
+})
+
+
+app.use('/api/v1/tasks',tasks)
+
+
+
+const port= 3002;
+
+app.listen(port,console.log(`Server is listening on the port ${port}`))
